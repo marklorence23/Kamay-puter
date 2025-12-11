52059c1b6405adbc20a879d0ed52f1dbbf37637e
marklorence23 <157224948+marklorence23@users.noreply.github.com>
2025-12-11 22:48:11 +0800

    Delete public directory

--- public/script.js
+++
@@ -1,6 +0,0 @@
-
-const menuToggle = document.querySelector(".menu-toggle");
-menuToggle.addEventListener("click", () => {
-  const menu = document.getElementById("navMenu");
-  menu.classList.toggle("show");
-});
